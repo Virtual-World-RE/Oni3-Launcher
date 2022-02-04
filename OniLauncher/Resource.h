1672d2c00774230fddeb9bb175e3792616622382
GGLinnk <gglinnk@protonmail.com>
2022-02-04 10:32:23 +0100

    Small code adjustment

--- OniLauncher/Resource.h
+++ OniLauncher/Resource.h
@@ -3,38 +3,39 @@
 // Used by OniLauncher.rc
 //
 
-#define IDC_STATIC                      -1
+#define IDC_STATIC                          (-1)
+#define IDC_GITHUB_LINK                     110
 
-#define ID_MONITOR                      200
-#define ID_RESOLUTION                   201
-#define ID_REFRESH_RATE                 202
-#define ID_DISPLAY_MODE                 203
-#define ID_FRAME_PER_SECONDS            204
-#define ID_PLAY                         205
-#define ID_SAVE                         206
-#define ID_DEBUG                        207
+#define ID_MONITOR                          200
+#define ID_RESOLUTION                       201
+#define ID_REFRESH_RATE                     202
+#define ID_DISPLAY_MODE                     203
+#define ID_FRAME_PER_SECONDS                204
+#define ID_PLAY                             205
+#define ID_SAVE                             206
+#define ID_DEBUG                            207
 
-#define IDM_ABOUT                       300
-#define IDM_EXIT                        301
+#define IDM_ABOUT                           300
+#define IDM_EXIT                            301
 
-#define IDI_ONILAUNCHER                 400
-#define IDI_SMALL                       401
+#define IDI_ONILAUNCHER                     400
+#define IDI_SMALL                           401
 
-#define IDD_ABOUTBOX                    500
+#define IDD_ABOUTBOX                        500
 
-#define IDS_APP_TITLE                   600
-#define IDS_ONILAUNCHER                 601
-#define IDS_MONITOR                     602
-#define IDS_MONITOR_DESC                603
-#define IDS_RESOLUTION_DESC             604
-#define IDS_REFRESH_DESC                605
-#define IDS_DISPLAY_MODE_DESC           606
-#define IDS_DISPLAY_MODE_BORD           607
-#define IDS_DISPLAY_MODE_FULL           608
-#define IDS_DISPLAY_MODE_WIND           609
-#define IDS_DEBUG                       610
-#define IDS_SAVE                        611
-#define IDS_PLAY                        612
+#define IDS_APP_TITLE                       600
+#define IDS_ONILAUNCHER                     601
+#define IDS_MONITOR                         602
+#define IDS_MONITOR_DESC                    603
+#define IDS_RESOLUTION_DESC                 604
+#define IDS_REFRESH_DESC                    605
+#define IDS_DISPLAY_MODE_DESC               606
+#define IDS_DISPLAY_MODE_BORD               607
+#define IDS_DISPLAY_MODE_FULL               608
+#define IDS_DISPLAY_MODE_WIND               609
+#define IDS_DEBUG                           610
+#define IDS_SAVE                            611
+#define IDS_PLAY                            612
 
 #define IDS_NO_MONITOR_SELECTED_TITLE       613
 #define IDS_NO_MONITOR_SELECTED_MSG         614
